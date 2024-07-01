4b300708c79144d230dafeab321d74cf8f225d3b
unknown <timofeyivv@gmail.com>
2024-07-01 13:45:37 +0300

    Изменены функции для получения слова из списка

--- index.php
+++ index.php
@@ -2,26 +2,11 @@
 <?php
 exec('chcp 65001');
 
-function getwordcount ($filecontent) { //Считается количество слов через запятую в строке
-    $wordcount = 0;
-    for ($i = 0; $i < strlen($filecontent); $i++) {
-        if ($filecontent[$i] == ',') $wordcount++;
-    }
-    if ($wordcount != 0) $wordcount++;
-    return $wordcount;
-}
 
 function getword ($filecontent) { //Берется случайное слово в строке слов через запятую
-    $wordcount = getwordcount($filecontent);
-    $randomnumber = rand (0, $wordcount);
-    $word = "";
-    $currentwordnum = 0;
-
-    for ($i = 0; $i < strlen($filecontent); $i++) { //Считываем случайное слово
-        if ($filecontent[$i] == ',') $currentwordnum++;
-        if ($currentwordnum == $randomnumber && $filecontent[$i] != ',') $word .= $filecontent[$i];
-        
-    }
+    $wordsArr = explode(",", $filecontent);
+    $randomnumber = rand(0, count($wordsArr) - 1);
+    $word = $wordsArr[$randomnumber];
 
     if ($randomnumber != 0) { // Если это не первое слово убираем пробел в начале
         $word = substr($word, 2);
@@ -31,6 +16,7 @@ function getword ($filecontent) { //Берется случайное слово
 
 $filecontent = file_get_contents("words.txt");
 $word = getword($filecontent);
+
 
 
 ?>
